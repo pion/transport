1a299101de658fdf74a2998f1e689587d94000fd
Steffen Vogel <post@steffenvogel.de>
2023-04-18 14:08:12 +0200

    Make package REUSE compliant

--- dpipe/dpipe.go
+++ dpipe/dpipe.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // Package dpipe provides the pipe works like datagram protocol on memory.
 //
 // This package is mainly intended for testing and not for production!
