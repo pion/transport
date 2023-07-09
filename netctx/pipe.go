761492968713b552fa20bf9c44a76d7c76ca27a8
Daniel Mangum <georgedanielmangum@gmail.com>
2023-07-09 18:45:48 -0400

    Move ConnCtx to the netctx package
    Moves ConnCtx to the netctx package, which will be expanded to include
    additional wrapped network interfaces.
    
    Signed-off-by: Daniel Mangum <georgedanielmangum@gmail.com>

---
+++ netctx/pipe.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+package netctx
+
+import (
+	"net"
+)
+
+// Pipe creates piped pair of ConnCtx.
+func Pipe() (ConnCtx, ConnCtx) {
+	ca, cb := net.Pipe()
+	return New(ca), New(cb)
+}
