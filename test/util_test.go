a1b0b8eec85eb3c068eb234f428baed2afa65665
Eric Daniels <eric@erdaniels.com>
2024-07-22 10:39:06 -0400

    Add CheckRoutinesStrict

--- test/util_test.go
+++ test/util_test.go
@@ -4,6 +4,8 @@
 package test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 )
@@ -20,4 +22,38 @@ func TestCheckRoutines(t *testing.T) {
 	go func() {
 		time.Sleep(1 * time.Second)
 	}()
+}
+
+func TestCheckRoutinesStrict(t *testing.T) {
+	mock := &tbMock{TB: t}
+
+	// Limit runtime in case of deadlocks
+	lim := TimeOut(time.Second * 20)
+	defer lim.Stop()
+
+	// Check for leaking routines
+	report := CheckRoutinesStrict(mock)
+	defer func() {
+		report()
+		if len(mock.fatalfCalled) == 0 {
+			t.Error("expected Fatalf to be called")
+		}
+		if !strings.Contains(mock.fatalfCalled[0], "Unexpected routines") {
+			t.Error("expected 'Unexpected routines'")
+		}
+	}()
+
+	go func() {
+		time.Sleep(1 * time.Second)
+	}()
+}
+
+type tbMock struct {
+	testing.TB
+
+	fatalfCalled []string
+}
+
+func (m *tbMock) Fatalf(format string, args ...interface{}) {
+	m.fatalfCalled = append(m.fatalfCalled, fmt.Sprintf(format, args...))
 }
