68250b8a30c3c65eb56f17cfd0f8a2c26dc191b9
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:46:04 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- udp/conn.go
+++ udp/conn.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package udp provides a connection-oriented listener over a UDP PacketConn
